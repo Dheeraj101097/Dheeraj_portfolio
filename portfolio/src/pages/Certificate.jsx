7acd65c1b1fa2b95db4a98a3ed7ef72fa426ed08
Dheeraj101097 <devishree1005@gmail.com>
2025-03-08 12:28:15 +0530

    certificate added

--- portfolio/src/pages/Certificate.jsx
+++ portfolio/src/pages/Certificate.jsx
@@ -23,7 +23,7 @@ const Certificate = () => {
               </div>
               <div className="p-6">
                 <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-                  Vertex Innovate(2025)
+                  Vertex Innovate(2025)-Special Mention for Innovative Solution
                 </h5>
                 <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
                   Issued by: VIT Vellore
